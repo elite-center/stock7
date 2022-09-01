4983bd478ac3f82e3e163851b869c2b3818aada5
bangyuwen <bangyuwen@gmail.com>
2022-09-01 21:43:27 +0800

    add shareholder's meeting

--- src/crawler-finlab/main.py
+++ src/crawler-finlab/main.py
@@ -1,3 +1,4 @@
+import os
 import os.path
 from datetime import datetime
 
@@ -7,10 +8,10 @@ import requests
 from finlab import data
 from genericpath import isfile
 from loguru import logger
-import os
 
 FINLAB_TOKEN = os.environ["FINLAB_TOKEN"]
 TG_TOKEN = os.environ["TG_TOKEN"]
+
 
 def fetch_conference() -> pd.DataFrame:
     cache_path = "temp/investors_conference.csv"
@@ -23,15 +24,37 @@ def fetch_conference() -> pd.DataFrame:
     return info
 
 
-def format_msg(companies: list, today: str) -> str:
+def fetch_shareholders_meeting() -> pd.DataFrame:
+    cache_path = "temp/shareholders_meeting.csv"
+    if os.path.isfile(cache_path):
+        info = pd.read_csv(cache_path)
+    else:
+        data.login(FINLAB_TOKEN)
+        info = data.get("shareholders_meeting")
+        info.to_csv(cache_path)
+    return info
+
+
+def format_conference_msg(companies: list, today: str) -> str:
     if companies:
         companies_str = "\n\n".join(
             f"[{company['公司名稱']}](https://www.google.com/search?q={company['公司名稱']}&tbm=nws&tbs=qdr:d) `{company['召開法人說明會地點']}`\n{company['法人說明會擇要訊息']}"
             for company in companies
         )
     else:
-        companies_str = "系統發生問題，請參照[其他網站](http://www.money-link.com.tw/stxba/imwcontent0.asp?page=INVC1&ID=INVC1)"
+        companies_str = "系統發生問題，請參照[其他網站](https://goodinfo.tw/tw/StockHolderMeetingList.asp?MARKET_CAT=全部&INDUSTRY_CAT=全部&YEAR=近期召開"
     return f"{today} 法說會\n\n" + companies_str
+
+
+def format_shareholders_meeting_msg(companies: list, today: str) -> str:
+    if companies:
+        companies_str = "\n\n".join(
+            f"[{company['公司名稱']}](https://www.google.com/search?q={company['公司名稱']}&tbm=nws&tbs=qdr:d) `"
+            for company in companies
+        )
+    else:
+        companies_str = "今日無股東會召開"
+    return f"{today} 股東會\n\n" + companies_str
 
 
 def send_tg(msg: str):
@@ -55,7 +78,16 @@ def process_conference():
     info_today = info[info.date == today][["公司名稱", "召開法人說明會地點", "法人說明會擇要訊息"]].to_dict(
         "records"
     )
-    msg = format_msg(info_today, today)
+    msg = format_conference_msg(info_today, today)
+    logger.debug(msg)
+    send_tg(msg)
+
+
+def process_shareholders_meeting():
+    info = fetch_shareholders_meeting()
+    today = datetime.utcnow().strftime("%Y-%m-%d")
+    info_today = info[info.date == today][["公司名稱"]].to_dict("records")
+    msg = format_shareholders_meeting_msg(info_today, today)
     logger.debug(msg)
     send_tg(msg)
 
@@ -63,4 +95,5 @@ def process_conference():
 @functions_framework.http
 def main(request):
     process_conference()
+    process_shareholders_meeting()
     return "OK"
